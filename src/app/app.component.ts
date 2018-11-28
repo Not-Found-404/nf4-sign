0b8814819471ed7f4fa166ad0a274abff032d32e
BillowsTao <taobillows@163.com>
2018-11-29 00:07:54 +0800

    添加介绍视频提示信息

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, TemplateRef, Input, AfterViewInit, AfterContentInit } from '@angular/core';
+import { NzNotificationService } from 'ng-zorro-antd';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  @Input() template: any;
+
+  /* 初始化函数 */
+  ngOnInit() {
+    // 触发显示通知框按钮的点击事件
+    document.getElementById('videoTriggerBtn').click();
+  }
+
+  /* 构造函数 */
+  constructor(
+    // 导入通知信息服务
+    private notification: NzNotificationService
+  ) {
+  }
+
+  /* 通知显示函数 */
+  createBasicNotification(template: TemplateRef<{}>): void {
+    this.notification.template(template);
+  }
+
+  /* 跳转观看视频函数 */
+  jumpToVideo(): void {
+    /* 跳转到优酷视频播放界面 */
+    window.open('http://v.youku.com/v_show/id_XMzkzODUyOTI3Mg==.html', '_blank');
+    // 移除通知框
+    this.notification.remove();
+  }
 }
